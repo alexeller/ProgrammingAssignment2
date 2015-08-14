87999b33362b19e1db0ca764da2c3232e9e2b59f
Alex Eller <kron_tab@hotmail.com>
2015-08-14 08:05:04 -0500

    tweaked indenting

--- cachematrix.R
+++ cachematrix.R
@@ -7,18 +7,17 @@
 ## The inverse can be set at a later point
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setinv <- function(inv) m <<- inv
-        getinv <- function() m
-        list(set = set, get = get,
-             setinv = setinv,
-             getinv = getinv)
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m <<- inv
+    getinv <- function() m
+    list(set = set, get = get,
+        setinv = setinv,
+        getinv = getinv)
 }
 
 
@@ -27,15 +26,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## inverted matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-         m <- x$getinv()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinv(m)
-        m
-
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.nul(m)) {
+    	    message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
